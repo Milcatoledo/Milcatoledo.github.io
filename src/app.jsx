d4df7b751468f2a1525d6481f86568fe11cef114
mika <mtoledom@unemi.edu.ec>
2026-03-20 09:58:01 -0500

    fix: remove broken contact route to fix white screen on github pages

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/home.jsx"
 import Projects from "./pages/projects.jsx"
 import Courses from "./pages/courses.jsx"
 import Events from "./pages/events.jsx"
-import Contact from "./pages/contact.jsx"
 
 function App() {
     return (
@@ -12,7 +11,6 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/courses" element={<Courses />} />
             <Route path="/events" element={<Events />} />
-            <Route path="/contact" element={<Contact />} />        
         </Routes>
     )
 }
